3e965d35631bf1815108bca4122559d276f12049
swierk434 <swierkpl@gmail.com>
2024-02-01 02:29:35 +0100

    commit2

--- src/Madelbrot/Mandelbrot2.java
+++ src/Madelbrot/Mandelbrot2.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Mandelbrot {
+public class Mandelbrot2 {
 	public static void main(String[] args) throws IOException {
 		double sizes[] = {16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
         double cores = Runtime.getRuntime().availableProcessors();
@@ -13,18 +13,18 @@ public class Mandelbrot {
 		for(int n = 0; n < sizes.length; n++) {
 			I[n] = new Image((int) sizes[n], (int) sizes[n], -2.1, 0.6, -1.2, 1.2, 200);
 			System.out.print('[');
-			for(int m = 0; m < 1; m++){
-				long start = System.nanoTime();
+			for(int m = 0; m < 10; m++){
 				MyThread[] Threads = new MyThread[(int) cores];
 				for(double x = 0; x < Runtime.getRuntime().availableProcessors(); x++){
                 	int xmin = (int) Math.ceil((x*(sizes[n]/cores)));
                 	int xmax = (int) Math.ceil((x+1)*(sizes[n]/cores));  
                 	//System.out.println(xmin + " " + xmax);
 					Threads[(int)x] =  new MyThread(String.valueOf((int)x),I[n], xmin, xmax, 0, (int) sizes[n]);
-            	} 	
+            	}
+				long start = System.nanoTime();
 				for(MyThread t : Threads){
 					t.start();
-				}
+				}	
 				for(MyThread t : Threads){
 					try {
 						t.join();
